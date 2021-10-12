95b630a6784db9ee689eda9726a7f105e738a822
kazutotaniyoshi <it352032@gmail.com>
2021-10-12 09:27:57 +0000

    test

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,11 +12,18 @@ class PostImagesController < ApplicationController
   end
 
   def index
+     @post_images = PostImage.all
   end
 
   def show
   end
 
   def destroy
+  end
+
+    private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f2140a35e1054f0b4a5a8affaca17632009e5d20880721345673342add595e111c4b11923dcc71ff346db5f10f78e0c29d4d62f23a54cdabd333067dff955521'
