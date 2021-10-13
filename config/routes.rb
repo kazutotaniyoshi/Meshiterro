fa9d096ff063b9096bff1450c3c1d3f816fcddc9
kazutotaniyoshi <it352032@gmail.com>
2021-10-13 06:54:47 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]do
+  resources :post_comments, only: [:create, :destroy]
+  end
 root to:'homes#top'
 end
